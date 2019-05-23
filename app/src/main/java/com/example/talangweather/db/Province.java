db630b06760e5093696e3cee359beb9bc0a2912c
huangguojia <474709968@qq.com>
2019-05-23 17:11:44 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/talangweather/db/Province.java
@@ -0,0 +1,33 @@
+package com.example.talangweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String privinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrivinceName() {
+        return privinceName;
+    }
+
+    public void setPrivinceName(String privinceName) {
+        this.privinceName = privinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
