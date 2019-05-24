0b8ac57840eda381af0ec6bbc2d33ea7baf87d0b
huangguojia <474709968@qq.com>
2019-05-24 22:39:31 +0800

    展示天气

--- app/src/main/java/com/example/talangweather/MainActivity.kt
+++ app/src/main/java/com/example/talangweather/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.talangweather
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 }
