66f649b9e8678ca95e46c81c16665f01e989f779
huangguojia <474709968@qq.com>
2019-05-26 19:00:07 +0800

    获取背景图片

--- app/src/main/java/com/example/talangweather/WeatherActivity.java
+++ app/src/main/java/com/example/talangweather/WeatherActivity.java
@@ -1,6 +1,9 @@
 package com.example.talangweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import android.view.View;
 import android.widget.*;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.bumptech.glide.Glide;
 import com.example.talangweather.gson.Weather;
 import com.example.talangweather.util.HttpUtil;
 import com.example.talangweather.util.Utility;
@@ -22,8 +26,7 @@ public class WeatherActivity extends AppCompatActivity {
 
 
 
-
-
+    private ImageView bingPicImg;
     private ScrollView weatherLayout;
     private TextView titleCity;
     private TextView titleUpdateTime;
@@ -36,31 +39,46 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= 21){ //只有当版本号大于或等于21时才会执行
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         ButterKnife.bind(this);
-        RelativeLayout titleView = (RelativeLayout)findViewById(R.id.title_layout);
-         titleCity =(TextView)titleView.findViewById(R.id.title_city);
-         titleUpdateTime=(TextView)findViewById(R.id.title_update_time);
-         degreeText=(TextView)findViewById(R.id.degree_text);
-         weatherInfoText=(TextView)findViewById(R.id.weather_info_text);
-         comforText=(TextView)findViewById(R.id.comfort_text);
-         carWashText=(TextView)findViewById(R.id.car_wash_text);
-         suportText=(TextView)findViewById(R.id.sport_text);
-         forecastLayout=(LinearLayout)findViewById(R.id.forecast_layout);
-         weatherLayout =(ScrollView)findViewById(R.id.weather_layout);
+        RelativeLayout titleView = (RelativeLayout) findViewById(R.id.title_layout);
+        titleCity = (TextView) titleView.findViewById(R.id.title_city);
+        titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
+        degreeText = (TextView) findViewById(R.id.degree_text);
+        weatherInfoText = (TextView) findViewById(R.id.weather_info_text);
+        comforText = (TextView) findViewById(R.id.comfort_text);
+        carWashText = (TextView) findViewById(R.id.car_wash_text);
+        suportText = (TextView) findViewById(R.id.sport_text);
+        forecastLayout = (LinearLayout) findViewById(R.id.forecast_layout);
+        weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
+        bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String weatherString = prefs.getString("weather", null);
-        if (weatherString != null){
-            Weather weather = Utility.handleWeatherResponse(weatherString);
-            showWeatherInfo(weather);
-        }else {
+        String bingPic = prefs.getString("bing_pic", null);
+        if (bingPic != null) {
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        } else {
+            loadBingPic();
+        }
+//        String weatherString = prefs.getString("weather", null);
+//        if (weatherString != null){
+//            Weather weather = Utility.handleWeatherResponse(weatherString);
+//            showWeatherInfo(weather);
+//        }else {
+//    }
             //无缓存时去服务器查询天气
             String weatherId = getIntent().getStringExtra("weather_Id");
             //可知butterknife的实例化在ncreate之后。
        //     weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
         }
-    }
+
 
     /**
      * 根据天气id请求城市天气信息
@@ -143,5 +161,38 @@ public class WeatherActivity extends AppCompatActivity {
             suportText.setText(suport);
         }
         weatherLayout.setVisibility(View.VISIBLE);
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(WeatherActivity.this, MyMainActivity.class);
+        startActivity(intent);
+    }
+
+    private void loadBingPic(){
+        String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
+                    }
+                });
+            }
+        });
     }
 }
